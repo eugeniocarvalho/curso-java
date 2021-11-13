280be02dbadd732d7dc198cb8af822dd4d3dbdd2
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2021-11-13 15:25:38 -0300

    string test

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,20 @@
+
+public class TestaCaracteres {
+	public static void main(String[] args) {
+		char letra = 'a';
+		
+		System.out.println("A char tem: '" + letra + "'");
+		
+		char valor = 66;
+		
+		System.out.println("char como valor númerico 66 é: " + valor);
+		
+		valor = (char) (valor + 1);
+		
+		System.out.println("char como valor númerico 67 é: " + valor);
+		
+		String umaFrase = "Olá, eu sou o Goku!";
+		
+		System.out.println("A frase é: \"" + umaFrase + "\"");
+	}
+}
