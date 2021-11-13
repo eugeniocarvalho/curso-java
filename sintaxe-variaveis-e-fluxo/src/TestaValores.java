451201438433215d1e2745caea5829ac552c46d1
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2021-11-13 15:53:45 -0300

    values test

---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,15 @@
+
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		int primeiroNumero = 5;
+		int segundoNumero = 7;
+		
+		segundoNumero = primeiroNumero;
+		primeiroNumero = 10;
+		
+		// quanto vale o segundo?
+		
+		System.out.println(segundoNumero);
+	}
+}
