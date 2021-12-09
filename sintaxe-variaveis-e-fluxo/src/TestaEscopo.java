ce7d4cbd48840d52a9cbf3baf6f16a80e0b28dee
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2021-12-09 08:59:42 -0300

    create TestaEscopo

---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,24 @@
+
+public class TestaEscopo {
+  public static void main(String[] args) {
+	  System.out.println("Testando condicional");
+	  
+	  int idade = 20;
+	  int quantidadePessoas = 3;
+	  //boolean acompanhado = quantidadePessoas >= 2;
+	  boolean acompanhado;
+	  
+	  if (quantidadePessoas >= 2)
+		  acompanhado = true;
+	  else
+		  acompanhado = false;
+	  
+	  System.out.println("valor de acompanhado " + acompanhado);
+	  
+	  if (idade >= 18 || acompanhado)
+		  System.out.println("Permitido a entrada");
+	  else
+		  System.out.println("Negada a entrada");
+
+  }
+}
