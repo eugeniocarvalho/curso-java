d48602f7a81e42e006a1e5e240fa97e3755952e6
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2021-12-09 09:28:48 -0300

    created TesteIR

--- sintaxe-variaveis-e-fluxo/src/TesteIR.java
+++ sintaxe-variaveis-e-fluxo/src/TesteIR.java
@@ -1,9 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TesteIR {
 	public static void main(String[] args) {
 		double salario = 1900;
 		double calculoImposto = 0;
-
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("MM");
+        
+        System.out.println(date.format(LocalDateTime.now()));
+        
 		if (salario <= 2800)
 			calculoImposto = salario * 0.925;
 		else if (salario <= 3751)
@@ -11,6 +16,6 @@ public class TesteIR {
 		else if (salario <= 4664)
 			calculoImposto = salario * 0.775;
 
-		System.out.println("Imposto é: " + calculoImposto);
+		System.out.println("Imposto calculado: " + calculoImposto);
 	}
 }
